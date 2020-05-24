e296e25a3a5920d38fb92abdafc7b34ad9452fe2
Bancho Petrov <banchopetrov@gmail.com>
2020-05-24 12:35:29 +0200

    Added React Navigation

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const HelloScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.content}>Welcome to BANISTAT!</Text>
+  </View>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>Welcome to BANISTAT!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Hello" component={HelloScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
